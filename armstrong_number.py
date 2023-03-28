e2eafb4e7085fb3ac5aa1daa6f75ceb7d88042f8
Odugu Dhanunjaya <dodugu@AD.MSYSTECHNOLOGIES.COM>
2023-03-28 17:18:48 +0530

    file added

---
+++ armstrong_number.py
@@ -0,0 +1,14 @@
+def armstrong(num):
+    l = len(str(num))
+    temp = num
+    arm = 0
+    while num > 0:
+        n = num % 10
+        arm = arm + n ** l
+        num = num //10
+    if temp == arm:
+        print("number is armstrong")
+    else:
+        print("number is not armstrong")
+
+armstrong(153)
